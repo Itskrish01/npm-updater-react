3cb9ad8cc95ed628e84843da332cc3a13a81b606
itsKrish01 <72066631+Itskrish01@users.noreply.github.com>
2024-07-19 11:27:52 +0530

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Toaster } from "react-hot-toast";
 import Header from "./components/Header";
 import Updater from "./components/Updater";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Toaster position="top-center" reverseOrder={false} />
       <Header />
       <Updater />
+      <Footer />
     </>
   );
 }
